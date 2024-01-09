8603b5995e1495774abda67498377f649e715925
ncgc <natalia.datascience@gmail.com>
2024-01-09 14:45:17 -0300

    first commit

---
+++ server-v2.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":80", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s years old %s", name, age)
+}
