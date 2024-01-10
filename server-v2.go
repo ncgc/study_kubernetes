3785f3dac84f292490068757eb8748f6930ca2b1
ncgc <natalia.datascience@gmail.com>
2024-01-10 12:10:17 -0300

    Inject configmap

--- server-v2.go
+++ server-v2.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 
 func main() {
+	http.HandleFunc("/config", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
@@ -11,4 +12,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s years old %s", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request){
+	data, err := ioutil.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "MyFamily :%s.", string(data))
 }
